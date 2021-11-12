0f2deedef0e437216db1815fa3db6da75a636731
casqade <ivamspasibo@gmail.com>
2021-11-12 20:10:03 +0300

    fix: GameState virtual methods log function+line without ':'

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -7,19 +7,19 @@ extern Logger Log;
 void
 GameState::enter()
 {
-  Log << __FUNCTION__ + std::to_string(__LINE__);
+  Log << __FUNCTION__ + ":"_s + std::to_string(__LINE__);
 }
 
 void
 GameState::render( sf::RenderWindow& )
 {
-  Log << __FUNCTION__ + std::to_string(__LINE__);
+  Log << __FUNCTION__ + ":"_s + std::to_string(__LINE__);
 }
 
 void
 GameState::update( const sf::Time )
 {
-  Log << __FUNCTION__ + std::to_string(__LINE__);
+  Log << __FUNCTION__ + ":"_s + std::to_string(__LINE__);
 }
 
 void
@@ -47,17 +47,17 @@ GameState::handleEvent( const sf::Event event )
 void
 GameState::keyEvent( const sf::Event::KeyEvent )
 {
-  Log << __FUNCTION__ + std::to_string(__LINE__);
+  Log << __FUNCTION__ + ":"_s + std::to_string(__LINE__);
 }
 
 void
 GameState::mouseMoveEvent( const sf::Event::MouseMoveEvent )
 {
-  Log << __FUNCTION__ + std::to_string(__LINE__);
+  Log << __FUNCTION__ + ":"_s + std::to_string(__LINE__);
 }
 
 void
 GameState::mouseButtonEvent( const sf::Event::MouseButtonEvent )
 {
-  Log << __FUNCTION__ + std::to_string(__LINE__);
+  Log << __FUNCTION__ + ":"_s + std::to_string(__LINE__);
 }
