f29c692445a2a4c2a1e5d08f63923b17a6b7634c
casqade <ivamspasibo@gmail.com>
2021-12-16 19:53:26 +0300

    add: Graphics3D Transformable class

---
+++ lib/Graphics3D/include/Graphics3D/Transformable.hpp
@@ -0,0 +1,45 @@
+#ifndef GRAPHICS_3D_TRANSFORMABLE_HPP
+#define GRAPHICS_3D_TRANSFORMABLE_HPP
+
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+
+namespace Graphics3D
+{
+
+class Transformable
+{
+protected:
+  glm::vec3 mOrigin;
+  glm::quat mOrientation;
+  glm::vec3 mScale;
+
+public:
+  Transformable(  const glm::vec3& origin = {},
+                  const glm::quat& orientation = glm::vec3{},
+                  const glm::vec3& scale = { 1.0f, 1.0f, 1.0f } );
+
+  virtual ~Transformable() = default;
+
+  virtual glm::mat4 model() const;
+
+  void translate( const glm::vec3& );
+  void rotate( const glm::quat& );
+  void rotateGlobal( const glm::quat& );
+  void scale( const glm::vec3& );
+
+  void setOrigin( const glm::vec3& );
+  void setOrientation( const glm::quat& );
+  void setScale( const glm::vec3& );
+
+  glm::vec3 origin() const;
+  glm::quat orientation() const;
+  glm::vec3 scale() const;
+};
+
+} // namespace Graphics3D
+
+
+#endif
---
+++ lib/Graphics3D/src/Transformable.cpp
@@ -0,0 +1,88 @@
+#include <Graphics3D/Transformable.hpp>
+
+
+namespace Graphics3D
+{
+
+Transformable::Transformable(
+  const glm::vec3& origin,
+  const glm::quat& orientation,
+  const glm::vec3& scale )
+  : mOrigin(origin)
+  , mOrientation(orientation)
+  , mScale(scale)
+{}
+
+glm::mat4
+Transformable::model() const
+{
+//  glm::mat4 translate = glm::translate( glm::mat4(1.0f), mOrigin );
+//  glm::mat4 rotation = glm::orientate4( mOrientation );
+//  glm::mat4 scale = glm::scale( mScale );
+
+  return  glm::translate( glm::mat4(1.0f), mOrigin )
+        * glm::toMat4(mOrientation)
+        * glm::scale( glm::mat4(1.0f), mScale );
+}
+
+void
+Transformable::translate( const glm::vec3& translation )
+{
+  mOrigin += translation;
+}
+
+void
+Transformable::rotate( const glm::quat& rotation )
+{
+  mOrientation *= rotation;
+}
+
+void
+Transformable::rotateGlobal( const glm::quat& rotation )
+{
+  mOrientation = rotation * mOrientation;
+}
+
+void
+Transformable::scale( const glm::vec3& scale )
+{
+  mScale += scale;
+}
+
+void
+Transformable::setOrigin( const glm::vec3& origin )
+{
+  mOrigin = origin;
+}
+
+void
+Transformable::setOrientation( const glm::quat& orientation )
+{
+  mOrientation = orientation;
+}
+
+void
+Transformable::setScale( const glm::vec3& scale )
+{
+  mScale = scale;
+}
+
+glm::vec3
+Transformable::origin() const
+{
+  return mOrigin;
+}
+
+glm::quat
+Transformable::orientation() const
+{
+  return mOrientation;
+}
+
+glm::vec3
+Transformable::scale() const
+{
+  return mScale;
+}
+
+} // namespace Graphics3D
