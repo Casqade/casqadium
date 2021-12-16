3ba81a04fd6051ec3599c10739ec2b8ce0a96660
casqade <ivamspasibo@gmail.com>
2021-12-16 19:53:00 +0300

    add: Graphics3D Camera implementation

--- lib/Graphics3D/include/Graphics3D/Camera.hpp
+++ lib/Graphics3D/include/Graphics3D/Camera.hpp
@@ -1,13 +1,53 @@
-#ifndef 3D_UTILS_CAMERA_HPP
-#define 3D_UTILS_CAMERA_HPP
+#ifndef GRAPHICS_3D_CAMERA_HPP
+#define GRAPHICS_3D_CAMERA_HPP
+
+#include <Graphics3D/Transformable.hpp>
 
 
 namespace Graphics3D
 {
 
-class Camera
+class Camera : public Transformable
 {
+public:
+  enum class Projection : bool
+  {
+    Perspective,
+    Orthogonal
+  };
 
+private:
+  float mFov;
+  glm::vec4 mViewport;
+  Projection mProjection;
+  std::pair <float, float> mZrange;
+
+  void setScale( const glm::vec3& ) = delete;
+  void scale( const glm::vec3& ) = delete;
+  glm::vec3 scale() const = delete;
+
+public:
+  Camera( const Projection& = Projection::Perspective,
+          const float fov = 45.0f,
+          const glm::vec4& viewport = {0.0f, 0.0f, 800.0f, 600.0f},
+          const std::pair <float, float>& zRange = {0.1f, 1000.0f} );
+
+  void setFov( const float degrees );
+  void setViewport( const glm::vec4& );
+  void setProjection( const Projection& );
+  void setZrange( const std::pair <float, float>& );
+
+  glm::vec3 front() const;
+  glm::vec3 right() const;
+  glm::vec3 up() const;
+
+  glm::mat4 viewMatrix() const;
+  glm::mat4 projMatrix() const;
+  glm::vec4 viewport() const;
+
+  float fov() const;
+  Projection projection() const;
+  std::pair <float, float> zRange() const;
 };
 
 } // namespace Graphics3D
--- lib/Graphics3D/src/Camera.cpp
+++ lib/Graphics3D/src/Camera.cpp
@@ -4,4 +4,100 @@
 namespace Graphics3D
 {
 
+Camera::Camera(
+  const Projection& projection,
+  const float fov,
+  const glm::vec4& viewport,
+  const std::pair <float, float>& zRange )
+  : mFov(fov)
+  , mViewport(viewport)
+  , mProjection(projection)
+  , mZrange(zRange)
+{}
+
+void
+Camera::setFov( const float degrees )
+{
+  mFov = degrees;
+}
+
+void
+Camera::setViewport( const glm::vec4& viewport )
+{
+  mViewport = viewport;
+}
+
+void
+Camera::setProjection( const Projection& projection )
+{
+  mProjection = projection;
+}
+
+void
+Camera::setZrange( const std::pair <float, float>& zRange )
+{
+  mZrange = zRange;
+}
+
+glm::vec3
+Camera::front() const
+{
+  return glm::rotate( mOrientation, {0.0f, 0.0f, -1.0f} );
+}
+
+glm::vec3
+Camera::right() const
+{
+  return glm::rotate( mOrientation, {1.0f, 0.0f, 0.0f} );
+}
+
+glm::vec3
+Camera::up() const
+{
+  return glm::rotate( mOrientation, {0.0f, 1.0f, 0.0f} );
+}
+
+glm::mat4
+Camera::viewMatrix() const
+{
+  return glm::lookAt( mOrigin,
+                      mOrigin + front(),
+                      up() );
+}
+
+glm::mat4
+Camera::projMatrix() const
+{
+  return  mProjection == Projection::Perspective ?
+          glm::perspectiveFovRH_ZO( glm::radians(mFov),
+                    mViewport.x, mViewport.y,
+                    mZrange.first, mZrange.second )
+        : glm::ortho( 0.0f, mViewport.x, 0.0f, mViewport.y,
+                      mZrange.first, mZrange.second );
+}
+
+glm::vec4
+Camera::viewport() const
+{
+  return mViewport;
+}
+
+float
+Camera::fov() const
+{
+  return mFov;
+}
+
+Camera::Projection
+Camera::projection() const
+{
+  return mProjection;
+}
+
+std::pair <float, float>
+Camera::zRange() const
+{
+  return mZrange;
+}
+
 } // namespace Graphics3D
