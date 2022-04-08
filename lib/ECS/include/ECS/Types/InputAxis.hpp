b078c654284d3bc57c95687982f12f49e87c93ce
casqade <ivamspasibo@gmail.com>
2022-04-09 00:50:11 +0300

    add: ECS InputAxis type

---
+++ lib/ECS/include/ECS/Types/InputAxis.hpp
@@ -0,0 +1,33 @@
+#ifndef ECS_TYPES_INPUT_AXIS_HPP
+#define ECS_TYPES_INPUT_AXIS_HPP
+
+#include <ECS/Alias.hpp>
+
+#include <set>
+#include <string>
+
+
+namespace ECS
+{
+
+namespace Types
+{
+
+struct InputAxis
+{
+  float value;
+  float sensitivity;
+  std::pair <float, float> constraint;
+
+  std::set <InputCallbackId> callbacks;
+
+
+  static std::string  toJson( const InputAxis& );
+  static InputAxis    fromJson( const std::string& );
+};
+
+} // namespace Types
+
+} // namespace ECS
+
+#endif
---
+++ lib/ECS/src/Types/InputAxis.cpp
@@ -0,0 +1,23 @@
+#include <ECS/Types/InputAxis.hpp>
+
+
+namespace ECS
+{
+
+namespace Types
+{
+std::string
+InputAxis::toJson( const InputAxis& )
+{
+  return {};
+}
+
+InputAxis
+InputAxis::fromJson( const std::string& json )
+{
+  return {};
+}
+
+} // namespace Types
+
+} // namespace ECS
