3b316d1ec291133ba1bd87cad48ecabb819daf2f
casqade <ivamspasibo@gmail.com>
2022-04-09 00:27:48 +0300

    add: ECS Alias.hpp

---
+++ lib/ECS/include/ECS/Alias.hpp
@@ -0,0 +1,19 @@
+#ifndef ECS_ALIAS_HPP
+#define ECS_ALIAS_HPP
+
+#include <entt/core/hashed_string.hpp>
+
+
+namespace ECS
+{
+
+typedef entt::hashed_string HardwareInputId;
+typedef entt::hashed_string CallbackId;
+typedef entt::hashed_string EntityId;
+typedef entt::hashed_string InputId;
+typedef entt::hashed_string InputCallbackId;
+typedef entt::hashed_string TextureId;
+
+} // namespace ECS
+
+#endif
