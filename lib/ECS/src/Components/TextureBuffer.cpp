26b014cc295b8bb2c8808a735fbaab4dea2756ee
casqade <ivamspasibo@gmail.com>
2022-04-09 00:56:49 +0300

    fix: ECS TextureBuffer component missing method definitions

--- lib/ECS/src/Components/TextureBuffer.cpp
+++ lib/ECS/src/Components/TextureBuffer.cpp
@@ -14,6 +14,31 @@ TextureBuffer::TextureBuffer()
   textures.reserve(2);  // front+back faces
 }
 
+void
+TextureBuffer::serialize( const std::string& json ) const
+{}
+
+void
+TextureBuffer::deserialize(
+  entt::registry& registry,
+  entt::entity entity,
+  const std::string& content ) const
+{
+  auto& comp = registry.emplace <TextureBuffer> (entity);
+}
+
+void
+TextureBuffer::Register()
+{
+  using namespace entt::literals;
+
+  auto factory = entt::meta <TextureBuffer> ();
+  factory.type(entt::hashed_string{"TextureBuffer"});
+  factory.data <&TextureBuffer::textures> ("TextureBuffer"_hs);
+  factory.func <&TextureBuffer::serialize> ("serialze"_hs);
+  factory.func <&TextureBuffer::deserialize> ("deserialze"_hs);
+}
+
 } // namespace Components
 
 } // namespace ECS
