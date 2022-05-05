27d816bdc728231f9c7d386563356204d0945bf8
casqade <ivamspasibo@gmail.com>
2022-05-05 19:41:03 +0300

    upd: more explicit call to olcPGE constructor

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@ main( int , char*[] )
 {
   AnotherDayAtHospital game( 30, 60 );
 
-  const olc::rcode result = game.Construct( 800, 600, 1, 1 );
+  const olc::rcode result = game.Construct( 800, 600, 1, 1,
+                                            false, false, true );
   if ( result != olc::rcode::OK )
     return result;
 
