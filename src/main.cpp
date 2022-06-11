fb1d61a7db2ca7e04de548cbb58eb4046d24ef16
casqade <ivamspasibo@gmail.com>
2022-06-11 18:24:37 +0400

    fix: undefined reference to spdlog sink on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <AnotherDayAtHospital.hpp>
 
-#include <spdlog/sinks/ansicolor_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/basic_file_sink.h>
 
 #include <cqde/util/ConfigManager.hpp>
 #include <cqde/util/logger.hpp>
+
 
 int
 main( int , char*[] )
@@ -16,12 +17,17 @@ main( int , char*[] )
 
   std::vector <std::shared_ptr <spdlog::sinks::sink>> sinks {};
 
+  auto stdErrSink = std::make_shared <spdlog::sinks::stderr_color_sink_mt> ();
+  stdErrSink->set_level(spdlog::level::level_enum::err);
+
+  sinks.push_back(stdErrSink);
+
   if ( configManager.logLevelCmd() < spdlog::level::level_enum::off )
   {
-    auto cmdSink = std::make_shared <spdlog::sinks::ansicolor_stdout_sink_mt> ();
-    cmdSink->set_level(configManager.logLevelCmd());
+    auto stdoutSink = std::make_shared <spdlog::sinks::stdout_color_sink_mt> ();
+    stdoutSink->set_level(configManager.logLevelCmd());
 
-    sinks.push_back(cmdSink);
+    sinks.push_back(stdoutSink);
   }
 
   if ( configManager.logLevelFile() < spdlog::level::level_enum::off )
