b85caa1533abcfc32b9b5d0676d7ca11574827ed
casqade <ivamspasibo@gmail.com>
2022-07-02 20:22:07 +0400

    fix: olc::Platform_Windows::olc_WindowEvent readability

--- lib/olcPGE/src/olcPixelGameEngine.cpp
+++ lib/olcPGE/src/olcPixelGameEngine.cpp
@@ -5,6 +5,7 @@
 #if defined(OLC_PLATFORM_WINAPI)
 
 #include <windows.h>
+#include <windowsx.h>
 #undef _WINSOCKAPI_
 
 #endif
@@ -3420,30 +3421,60 @@ namespace olc
       switch (uMsg)
         {
         case WM_MOUSEMOVE:
-          {
-            // Thanks @ForAbby (Discord)
-            uint16_t x = lParam & 0xFFFF; uint16_t y = (lParam >> 16) & 0xFFFF;
-            int16_t ix = *(int16_t*)&x;   int16_t iy = *(int16_t*)&y;
-            ptrPGE->olc_UpdateMouse(ix, iy);
-            return 0;
-          }
-        case WM_SIZE:       ptrPGE->olc_UpdateWindowSize(lParam & 0xFFFF, (lParam >> 16) & 0xFFFF);	return 0;
-        case WM_MOUSEWHEEL:	ptrPGE->olc_UpdateMouseWheel(GET_WHEEL_DELTA_WPARAM(wParam));           return 0;
-        case WM_MOUSELEAVE: ptrPGE->olc_UpdateMouseFocus(false);                                    return 0;
-        case WM_SETFOCUS:	ptrPGE->olc_UpdateKeyFocus(true);                                       return 0;
-        case WM_KILLFOCUS:	ptrPGE->olc_UpdateKeyFocus(false);                                      return 0;
-        case WM_KEYDOWN:	ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);                      return 0;
-        case WM_KEYUP:		ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);                     return 0;
-        case WM_SYSKEYDOWN: ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);						return 0;
-        case WM_SYSKEYUP:	ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);						return 0;
-        case WM_LBUTTONDOWN:ptrPGE->olc_UpdateMouseState(0, true);                                  return 0;
-        case WM_LBUTTONUP:	ptrPGE->olc_UpdateMouseState(0, false);                                 return 0;
-        case WM_RBUTTONDOWN:ptrPGE->olc_UpdateMouseState(1, true);                                  return 0;
-        case WM_RBUTTONUP:	ptrPGE->olc_UpdateMouseState(1, false);                                 return 0;
-        case WM_MBUTTONDOWN:ptrPGE->olc_UpdateMouseState(2, true);                                  return 0;
-        case WM_MBUTTONUP:	ptrPGE->olc_UpdateMouseState(2, false);                                 return 0;
-        case WM_CLOSE:		ptrPGE->olc_Terminate();                                                return 0;
-        case WM_DESTROY:	PostQuitMessage(0); DestroyWindow(hWnd);								return 0;
+          ptrPGE->olc_UpdateMouse(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+          return 0;
+        case WM_SIZE:
+          ptrPGE->olc_UpdateWindowSize(lParam & 0xFFFF, (lParam >> 16) & 0xFFFF);
+          return 0;
+        case WM_MOUSEWHEEL:
+          ptrPGE->olc_UpdateMouseWheel(GET_WHEEL_DELTA_WPARAM(wParam));
+          return 0;
+        case WM_MOUSELEAVE:
+          ptrPGE->olc_UpdateMouseFocus(false);
+          return 0;
+        case WM_SETFOCUS:
+          ptrPGE->olc_UpdateKeyFocus(true);
+          return 0;
+        case WM_KILLFOCUS:
+          ptrPGE->olc_UpdateKeyFocus(false);
+          return 0;
+        case WM_KEYDOWN:
+          ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);
+          return 0;
+        case WM_KEYUP:
+          ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);
+          return 0;
+        case WM_SYSKEYDOWN:
+          ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);
+          return 0;
+        case WM_SYSKEYUP:
+          ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);
+          return 0;
+        case WM_LBUTTONDOWN:
+          ptrPGE->olc_UpdateMouseState(0, true);
+          return 0;
+        case WM_LBUTTONUP:
+          ptrPGE->olc_UpdateMouseState(0, false);
+          return 0;
+        case WM_RBUTTONDOWN:
+          ptrPGE->olc_UpdateMouseState(1, true);
+          return 0;
+        case WM_RBUTTONUP:
+          ptrPGE->olc_UpdateMouseState(1, false);
+          return 0;
+        case WM_MBUTTONDOWN:
+          ptrPGE->olc_UpdateMouseState(2, true);
+          return 0;
+        case WM_MBUTTONUP:
+          ptrPGE->olc_UpdateMouseState(2, false);
+          return 0;
+        case WM_CLOSE:
+          ptrPGE->olc_Terminate();
+          return 0;
+        case WM_DESTROY:
+          PostQuitMessage(0);
+          DestroyWindow(hWnd);
+          return 0;
         }
       return DefWindowProc(hWnd, uMsg, wParam, lParam);
     }
