551e38bab182a1105a98ba0b9a1a81cdf37f471b
casqade <ivamspasibo@gmail.com>
2022-07-14 21:02:06 +0400

    fix: ImGuizmo legacy functions errors; imgui compile definition options

--- lib/olcPGE/src/olcPGEX_ImGui.cpp
+++ lib/olcPGE/src/olcPGEX_ImGui.cpp
@@ -57,7 +57,7 @@ olc::rcode PGE_ImGUI::ImGui_ImplPGE_Init()
   io.KeyMap[ImGuiKey_Space] = olc::SPACE;
   io.KeyMap[ImGuiKey_Enter] = olc::ENTER;
   io.KeyMap[ImGuiKey_Escape] = olc::ESCAPE;
-  io.KeyMap[ImGuiKey_KeyPadEnter] = olc::RETURN;
+  io.KeyMap[ImGuiKey_KeypadEnter] = olc::RETURN;
   io.KeyMap[ImGuiKey_A] = olc::A;
   io.KeyMap[ImGuiKey_C] = olc::C;
   io.KeyMap[ImGuiKey_V] = olc::V;
