392d3e7d0ccf4bf316e33d03bfb73e3cbf7115a3
casqade <ivamspasibo@gmail.com>
2022-08-29 02:35:51 +0300

    add: EntityMetaInfo [de]serialization implementation

--- lib/casqadium/src/components/EntityMetaInfo.cpp
+++ lib/casqadium/src/components/EntityMetaInfo.cpp
@@ -1,22 +1,52 @@
 #include <cqde/components/EntityMetaInfo.hpp>
 
+#include <cqde/json_helpers.hpp>
+
+#include <entt/entity/registry.hpp>
+
 #include <json/value.h>
 
 
 namespace cqde::compos
 {
 
+const static Json::Value entityMetaInfoJsonReference =
+[]
+{
+  using ValueType = Json::ValueType;
+  using namespace std::string_literals;
+
+  Json::Value root = ValueType::objectValue;
+  root.setComment("// JSON root must be an object"s,
+                   Json::CommentPlacement::commentBefore);
+
+  root["packageId"] = ValueType::stringValue;
+  root["packageId"].setComment("// 'packageId' must be a JSON string"s,
+                                Json::CommentPlacement::commentBefore);
+
+  return root;
+}();
+
 Json::Value
 EntityMetaInfo::serialize() const
 {
-  return Json::objectValue;
+  Json::Value json {};
+  json["packageId"] = packageId.str();
+
+  return json;
 }
 
 void
 EntityMetaInfo::deserialize(
-  entt::registry&,
-  entt::entity,
-  const Json::Value& )
-{}
+  entt::registry& registry,
+  entt::entity entity,
+  const Json::Value& json )
+{
+  jsonValidateObject(json, entityMetaInfoJsonReference);
+
+  auto& comp = registry.emplace <EntityMetaInfo> (entity);
+
+  comp.packageId = json["packageId"].asString();
+}
 
 } // namespace cqde::compos
