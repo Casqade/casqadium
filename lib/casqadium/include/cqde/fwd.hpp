3961561fc926496276cb058088bf48ad4d835c4f
casqade <ivamspasibo@gmail.com>
2022-09-06 23:15:35 +0300

    upd: cqde forward declarations

--- lib/casqadium/include/cqde/fwd.hpp
+++ lib/casqadium/include/cqde/fwd.hpp
@@ -10,10 +10,14 @@ namespace compos
 {
 
 struct Camera;
+struct EntityMetaInfo;
 struct GeometryBuffer;
 struct InputController;
 struct SceneNode;
-struct SequenceManager;
+struct SequenceController;
+struct SnapshotExcluded;
+struct SubscriberInput;
+struct SubscriberUpdate;
 struct Tag;
 struct TextureBuffer;
 struct Transform;
@@ -24,14 +28,40 @@ namespace types
 {
 
 struct EntityReference;
+struct FrameCurrent;
 struct ControlAxis;
 struct InputAxisComparator;
 struct InputBinding;
 struct InputBindingComparator;
-struct InputManager;
-struct SequenceStep;
+struct InputEvent;
+struct System;
+struct TickCurrent;
 struct VertexBuffer;
 
+class CallbackManager;
+class ConfigManager;
+class Package;
+class PackageManager;
+class SystemManager;
+
 } // namespace types
+
+namespace ui
+{
+
+class PackageFilter;
+class RegistryFilter;
+class StringFilter;
+class ToggleButton;
+
+class AssetManagerUi;
+class CallbackManagerUi;
+class EntityManagerUi;
+class InputManagerUi;
+class PackageManagerUi;
+class SystemManagerUi;
+class ViewportManagerUi;
+
+} // namespace ui
 
 } // namespace cqde
