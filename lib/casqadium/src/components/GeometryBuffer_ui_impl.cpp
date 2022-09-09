6f7915d3cc987fd18199e83df47f1f6ba4da1a09
casqade <ivamspasibo@gmail.com>
2022-09-09 13:42:22 +0300

    upd: GeometryBuffer::ui_edit_props now use StringFilter widget

--- lib/casqadium/src/components/GeometryBuffer_ui_impl.cpp
+++ lib/casqadium/src/components/GeometryBuffer_ui_impl.cpp
@@ -1,5 +1,6 @@
 #include <cqde/components/GeometryBuffer.hpp>
 
+#include <cqde/types/ui/widgets/StringFilter.hpp>
 #include <cqde/types/assets/GeometryAssetManager.hpp>
 
 #include <entt/entity/registry.hpp>
@@ -15,18 +16,28 @@ GeometryBuffer::ui_edit_props(
   const entt::entity,
   const entt::registry& registry )
 {
+  using ui::StringFilter;
   using types::GeometryAssetManager;
 
   if ( ImGui::CollapsingHeader("Buffer ID", ImGuiTreeNodeFlags_DefaultOpen) == false )
     return;
 
+  static StringFilter geometryFilter {"Geometry ID"};
+
   const auto assets = registry.ctx().at <GeometryAssetManager> ().assetIdList();
 
-  if ( ImGui::BeginCombo("##bufferId", buffer.str().c_str(),
-                         ImGuiComboFlags_HeightLargest) )
+  if ( ImGui::BeginCombo("##geometryId", buffer.str().c_str()) )
   {
+    if ( ImGui::IsWindowAppearing() )
+      ImGui::SetKeyboardFocusHere(2);
+
+    geometryFilter.search({}, ImGuiInputTextFlags_AutoSelectAll);
+
     for ( const auto& asset : assets )
     {
+      if ( geometryFilter.query(asset.str()) == false )
+        continue;
+
       const bool selected = (buffer == asset);
 
       if ( ImGui::Selectable(asset.str().c_str(), selected) )
