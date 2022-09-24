5b27e1809df86ece0ea3deb9069ecf1ed71d4cf8
casqade <ivamspasibo@gmail.com>
2022-09-24 17:27:02 +0300

    add: CasqadiumEditorCameraSettings serialization

--- lib/casqadium/src/components/CasqadiumEditorCameraSettings.cpp
+++ lib/casqadium/src/components/CasqadiumEditorCameraSettings.cpp
@@ -1,5 +1,7 @@
 #include <cqde/components/CasqadiumEditorCameraSettings.hpp>
 
+#include <cqde/json_helpers.hpp>
+
 #include <entt/entity/registry.hpp>
 
 #include <json/value.h>
@@ -8,21 +10,47 @@
 namespace cqde::compos
 {
 
+const static Json::Value casqadiumEditorCameraSettingsJsonReference =
+[]
+{
+  using ValueType = Json::ValueType;
+  using namespace std::string_literals;
+
+  Json::Value root = ValueType::objectValue;
+  root.setComment("// root must be a JSON object"s,
+                   Json::CommentPlacement::commentBefore);
+
+  auto& color = root["speed"];
+  color = ValueType::realValue;
+  color.setComment("// 'speed' must be a JSON float"s,
+                    Json::CommentPlacement::commentBefore);
+
+  return root;
+}();
+
 Json::Value
 CasqadiumEditorCameraSettings::serialize() const
 {
-  return Json::ValueType::objectValue;
+  Json::Value json {Json::objectValue};
+
+  json["speed"] = speed;
+
+  return json;
 }
 
 void
 CasqadiumEditorCameraSettings::deserialize(
   entt::registry& registry,
   entt::entity entity,
-  const Json::Value&,
+  const Json::Value& json,
   const std::unordered_map <EntityId, EntityId,
                             identifier_hash>& idMap )
 {
-  registry.emplace_or_replace <CasqadiumEditorCameraSettings> (entity);
+  jsonValidateObject(json, casqadiumEditorCameraSettingsJsonReference);
+
+  auto& comp = registry.emplace_or_replace <CasqadiumEditorCameraSettings> (entity);
+
+  comp.speed = json["speed"].asFloat();
 }
 
 } // namespace cqde::compos
